c87491fb6cf867dc550147872370f8541cf2f407
Marcel <marcel.kolenbrander@outlook.com>
2020-10-18 19:38:34 +0200

    Add files via upload

---
+++ motionToSound.js
@@ -0,0 +1,31 @@
+function motionToSoundHandler(event) {
+    motionHandler(event);
+
+    updatePitchActual(p_x * 10);
+    updateVolumeActual(p_y * 10);
+
+    updateMovementToSoundFields();
+}
+
+function movementToSoundPage() {
+    if ($("#slider-music-MTM").prop("checked")) {
+        requestMotionPermission();
+
+        if (volumeActual > 0)
+            soundRunning = true
+        if (soundRunning) {
+            synth.triggerAttack(pitchActual, now);
+            synth.volume.rampTo(volumeActual, 0.1);
+        }
+        soundEnabled = true;
+
+        res = window.addEventListener("devicemotion", motionToSoundHandler);
+
+        alert(res);
+    } else {
+        window.removeEventListener("devicemotion", motionToSoundHandler);
+        synth.triggerRelease(now);
+        soundEnabled = false;
+        soundRunning = false;
+    }
+}
